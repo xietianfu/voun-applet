046be0af98d8c70cd5f9e185b7154f8d450df249
xietf <826597263@qq.com>
2019-03-10 14:13:27 +0800

    feat: 完成相框与瀑布流页面

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,19 +1,92 @@
 <template>
-  <div>
-    <p class="card">
-      {{text}}
-    </p>
+  <div class="photo-wrap">
+    <image
+      class="bg-img"
+      :src="tableLampUrl"
+      mode="aspectFill"
+    ></image>
+    <div class="frame">
+      <image
+        class="bg-frame"
+        :src="frame"
+      ></image>
+      <image
+        class="photo"
+        :src="photo"
+        mode="aspectFill"
+        alt
+      ></image>
+
+    </div>
   </div>
 </template>
 
 <script>
+import tableLamp from "@/assets/images/scene/台灯桌面照片.png";
+import photo from "@/assets/images/照片.png";
+import frame1 from "@/assets/images/frame/相框1.png";
+
+wx.getImageInfo({
+  src:
+    "http://www.th7.cn/d/file/p/2014/11/07/d9803b88c468e915861965f0a8c58bc4.png",
+  success: res => console.log(res.width)
+});
+
 export default {
-  props: ['text']
-}
+  // props: ["wrapBackground", "frame", "photoUrl"],
+  data() {
+    return {
+      // wrapBackground: wrapBackground,
+      tableLampUrl: tableLamp,
+      photo: photo,
+      frame: frame1
+    };
+  }
+};
 </script>
 
-<style>
-.card {
-  padding: 10px;
+<style lang="less" scoped>
+.photo-wrap {
+  position: relative;
+  display: flex;
+  height: 430px;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+  background-size: 70%;
+  background-repeat: no-repeat;
+  box-shadow: -10pt 0pt 25px #b3b3b3;
+}
+.bg-img {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: -2;
+}
+.frame {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 80%;
+  height: 45%;
+  background: white;
+  box-shadow: 10px 5px 10px #b3b3b3;
+  .bg-frame {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+  .photo {
+    max-width: 100%;
+    max-height: 100%;
+    text-align: center;
+  }
 }
 </style>
+
---
+++ src/components/navBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="bar-warp">
+    <image :src="buy"></image>
+    <image :src="add"></image>
+    <image :src="vip"></image>
+  </div>
+</template>
+
+<script>
+import vip from "@/assets/images/ico/会员.png";
+import add from "@/assets/images/ico/添加.png";
+import buy from "@/assets/images/ico/购买.png";
+
+export default {
+  data() {
+    return {
+      vip: vip,
+      add: add,
+      buy: buy
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.bar-warp {
+  position: fixed;
+  display: flex;
+  bottom: 3%;
+  height: 65px;
+  z-index: 1000;
+  width: 100%;
+  justify-content: space-around;
+  align-content: flex-end;
+  align-items: flex-end;
+
+  image {
+    width: 50px;
+    height: 50px;
+    box-shadow: 9pt 7pt 18pt rgba(0, 0, 0, 0.2);
+    border-radius: 1000px;
+    &:nth-of-type(2) {
+      width: 65px;
+      height: 65px;
+    }
+  }
+}
+</style>
